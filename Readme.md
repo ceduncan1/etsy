f58399ff04158059fcd5271dd8242e25589b5b18
Cary Duncan <duncan.cary@gmail.com>
2015-10-08 15:18:21 -0400

    Completed JS and HTML, began SCSS

--- Readme.md
+++ Readme.md
@@ -1,3 +1,1 @@
-## This is a Readme
-
-> You should probably update me.
+## Etsy recreation
--- js/main.js
+++ js/main.js
@@ -1,5 +1,48 @@
-(function () {
+// (function () {
 
-  console.log('It Works!');
+// console.log(etsy);
+var items = etsy.results;
 
-}());
+var templateString = $('#itemTemplate').text(); //could use .html
+// console.log(templateString);
+
+var templateFunction = _.template(templateString); //.template "does the work for me" ..? makes a function, takes obj, returns template
+
+
+_.each(items,  function(item, index) {
+  // console.log(index);
+  if (index <= 23) {
+    // console.log(index);
+    var itemHTML = templateFunction(item);
+    $('.bodyR').append(itemHTML);
+  }
+});
+
+
+
+
+
+// var itemImages = etsy.results[0].Images[0].url_75x75;
+// console.log(itemImages);
+
+
+
+
+// HTML
+
+
+
+  // var firstPageItems = [];
+
+  // var listResults = function () {
+  //   if (etsy.results.indexOf() <= 47) {
+  //     firstPageItems.push(etsy.results);
+  //   }
+  // }
+
+  // console.log(firstPageItems);
+
+// }());
+
+
+
